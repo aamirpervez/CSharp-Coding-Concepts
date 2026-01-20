119fb2d5a41b65cb8f5daf12f9c6d8ad1ac52e33
Aamir Pervez <aamirpervez27@gmail.com>
2026-01-20 20:18:32 +0500

    Add files via upload

---
+++ AA_1 Simple Inheritance - Vehicle System/Inheritance vehicle system.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace Vehicle_System
+{
+    //base class
+    class Vehicle
+    {
+        public string Make { get; set; }
+        public string Modle { get; set; }
+        public ushort Year { get; set; }
+
+        public virtual void StartEngine()
+        {
+            Console.WriteLine("Engine Starting...");
+        }
+
+        public virtual void DisplayInfo()
+        {
+            Console.WriteLine($"Make: {Make}");
+            Console.WriteLine($"Modle: {Modle}");
+            Console.WriteLine($"Year: {Year}");
+        }
+    }
+
+    //Derived class inherit vehicle
+    class Car : Vehicle
+    {
+        public int NumberOfDoors { get; set; }
+
+        public override void StartEngine()
+        {
+            Console.WriteLine("Car Engine Starting...");
+        }
+
+        public override void DisplayInfo()
+        {
+            base.DisplayInfo();
+            Console.WriteLine($"Number of Doors: {NumberOfDoors}");
+        }
+    }
+
+    class Motorcycle : Vehicle
+    {
+        public bool HasSideCar { get; set; }
+
+        public override void StartEngine()
+        {
+            Console.WriteLine("Motorcycle Engine Starting...");
+        }
+
+        public override void DisplayInfo()
+        {
+            base.DisplayInfo();
+            Console.WriteLine($"Side Car: {HasSideCar}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car1 = new Car();
+            car1.Make = "Car";
+            car1.Year = 2015;
+            car1.Modle = "Honda";
+            car1.NumberOfDoors = 4;
+
+            Car car2 = new Car();
+            car2.Make = "Car";
+            car2.Year = 1992;
+            car2.Modle = "Supra Mk3";
+            car2.NumberOfDoors = 2;
+
+            Motorcycle bike1 = new Motorcycle();
+            bike1.Make = "Motorcycle";
+            bike1.Year = 2020;
+            bike1.Modle = "Honda 125";
+            bike1.HasSideCar = false;
+
+            car1.StartEngine();
+            car1.DisplayInfo();
+
+            Console.WriteLine();
+
+            car2.StartEngine();
+            car2.DisplayInfo();
+
+            Console.WriteLine();
+
+            bike1.StartEngine();
+            bike1.DisplayInfo();
+        }
+    }
+}
---
+++ AA_2 Bank Account with Transaction History/Bank Account and Transaction History.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace Bank_Account_Transaction_History
+{
+    class BankAccount
+    {
+        public int AccountNumber { get; set; }
+        private int Balance {  get; set; }
+
+        private List<string> TransactionHistory { get; set; } = new List<string>();
+
+        public void Deposit(int amount)
+        {
+            if (amount > 0)
+            {
+                Balance += amount;
+                TransactionHistory.Add($"{DateTime.Now:g}: Deposit {amount:c}. New Balance:{Balance:c}");
+                Console.WriteLine($"Amount of {amount:c} Successfully Deposited to: {AccountNumber}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid Amount!");
+            }
+        }
+        
+        public void Withdraw(int amount)
+        {
+            if (Balance >= amount)
+            {
+                Balance -= amount;
+                TransactionHistory.Add($"{DateTime.Now:g}: Withdrawal {amount:c}. New Balance:{Balance:c}");
+                Console.WriteLine($"Amount of {amount:c} Successfully Withdrawal From: {AccountNumber}");
+            }
+            else
+            {
+                Console.WriteLine("Insufficiant Balance!");
+            }
+        }
+
+        public void PrintStatement()
+        {
+            Console.WriteLine($"Account Number: {AccountNumber}");
+            Console.WriteLine($"Current Balance: {Balance}");
+            for (int i = 0; i < TransactionHistory.Count; i++)
+            {
+                Console.WriteLine(TransactionHistory[i]);
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount account1 = new BankAccount();
+            account1.AccountNumber = 123456;
+
+            BankAccount account2 = new BankAccount();
+            account2.AccountNumber = 987654;
+
+            account1.Deposit(10000);
+            Console.WriteLine();
+            account1.Withdraw(1000);
+            Console.WriteLine();
+            account1.PrintStatement();
+
+            Console.WriteLine();
+            account2.Withdraw(3000);
+            Console.WriteLine();
+            account2.PrintStatement();
+        }
+    }
+}
