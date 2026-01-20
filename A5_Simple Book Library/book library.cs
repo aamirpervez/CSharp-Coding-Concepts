705dc8bf0d82157eb29acbb27ae4aad959930fae
Aamir Pervez <aamirpervez27@gmail.com>
2026-01-20 20:17:39 +0500

    Added two projects
    A5_Simple Book Library , A6_Rectangle Area Calculator

---
+++ A5_Simple Book Library/book library.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Runtime.Intrinsics.X86;
+namespace book_library
+{
+    //Book class
+    class Book
+    {
+        //seting a book properties
+        public string Title { get; set; }
+        public string Author { get; set; }
+
+        // the book availablility true by default 
+        public bool IsAvailable { get;private set; } = true;
+
+        //method for borrowing book
+        public void BorrowBook()
+        {
+            // if book is available this part run
+            if (IsAvailable == true)
+            {
+                IsAvailable = false;
+                Console.WriteLine($"You Successfully Borrowed The Book of {Title}");
+            }
+            // if not this work
+            else
+            {
+                Console.WriteLine("This Book is Not Available");
+            }
+        }
+
+        // method for returing the book
+        public void ReturnBook()
+        {
+            IsAvailable = true;
+            Console.WriteLine($"Book of {Title} returned successfully");
+        }
+
+        // displying book info
+        public void DisplayStatus()
+        {
+            Console.WriteLine($"Book Title: {Title}");
+            Console.WriteLine($"Book Author: {Author}");
+            Console.WriteLine($"Book Availability: {IsAvailable}");
+        }
+    }
+
+    class Program
+    {
+        //main entry point method
+        static void Main( string[] args)
+        {
+            //creating 5 books and seting its properties
+            Book b1 = new Book();
+            b1.Title = "Shadow and Bone";
+            b1.Author = "Leigh Bardugo";
+
+            Book b2 = new Book();
+            b2.Title = "The Witcher: The Last Wish";
+            b2.Author = "Andrzej Sapkowski";
+
+            Book b3 = new Book();
+            b3.Title = "3 Body Problem";
+            b3.Author = "Cixin Liu";
+
+            Book b4 = new Book();
+            b4.Title = "Harry Potter and the Sorcerer’s Stone";
+            b4.Author = "J.K. Rowling";
+
+            Book b5 = new Book();
+            b5.Title = "The Da Vinci Code";
+            b5.Author = "Dan Brown";
+
+            //borrowing the book3 book2 book5 and then return the book3
+            b3.BorrowBook();
+            b5.BorrowBook();
+            b1.BorrowBook();
+            b3.ReturnBook();
+
+            //create a list to store all books in collection
+            List<Book> list = new List<Book>();
+            list.Add(b1);
+            list.Add(b2);
+            list.Add(b3);
+            list.Add(b4);
+            list.Add(b5);
+
+            //loop for list collection of books for checking one by one
+            for (int i = 0; i < list.Count; i++)
+            {
+                //only show the available books
+                if(list[i].IsAvailable == true)
+                {
+                    list[i].DisplayStatus();
+                }
+            }
+        }
+    }
+}
---
+++ A6_Rectangle Area Calculator/Rectangle Area Calculator.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Threading.Channels;
+
+namespace Rectangle_Area_Calculator
+{
+    //rectangle blueprint class
+    class Rectangle
+    {
+        //properties to get access from outside of class and store values
+        public double Length { get; private set; }
+        public double Width { get; private set; }
+
+        // constructor to store values while creating the object
+        // peremeterize constructor
+        public Rectangle(double length, double width)
+        {
+            Length = length;
+            Width = width;
+        }
+
+        // this method return the area of length and width by multiplying values
+        public double CalculateArea()
+        {
+            return Length * Width;
+        }
+
+        // this method return the perimeter of length and width by suming them and multiply by 2
+        public double CalculatePerimeter()
+        {
+            return 2 * (Length + Width);
+        }
+
+        // this method check is both values are equal if yes it return ture that this is square
+        public bool IsSquare()
+        {
+            if(Length == Width)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+
+    // main program class
+    class Program
+    {
+        // entry point method
+        static void Main(string[] args)
+        {
+            //create first rectangle and pass the value to its constructor
+            Rectangle rt1 = new Rectangle(2, 8);
+            Console.WriteLine($"First Rectangle Area: {rt1.CalculateArea()}");
+            Console.WriteLine($"First RectanglePerimeter: {rt1.CalculatePerimeter()}");
+            Console.WriteLine($"First Rectangle is Square: {rt1.IsSquare()}\n");
+
+            //create second rectangle and pass the value to its constructor
+            Rectangle rt2 = new Rectangle(7, 7);
+            Console.WriteLine($"Second Rectangle Area: {rt2.CalculateArea()}");
+            Console.WriteLine($"Second RectanglePerimeter: {rt2.CalculatePerimeter()}");
+            Console.WriteLine($"Second Rectangle is Square: {rt2.IsSquare()}\n");
+
+            //create thered rectangle and pass the value to its constructor
+            Rectangle rt3 = new Rectangle(6, 10);
+            Console.WriteLine($"Threed Rectangle Area: {rt3.CalculateArea()}");
+            Console.WriteLine($"Threed RectanglePerimeter: {rt3.CalculatePerimeter()}");
+            Console.WriteLine($"Threed Rectangle is Square: {rt3.IsSquare()}");
+
+
+        }
+    }
+}
