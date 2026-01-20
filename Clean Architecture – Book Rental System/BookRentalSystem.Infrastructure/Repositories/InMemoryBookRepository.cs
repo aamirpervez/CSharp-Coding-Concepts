63060c22975379bfc3a165cdce3b65af5400fb0e
Aamir Pervez <aamirpervez27@gmail.com>
2026-01-20 20:58:56 +0500

    Add files via upload

---
+++ Clean Architecture – Book Rental System/BookRentalSystem.Infrastructure/Repositories/InMemoryBookRepository.cs
@@ -0,0 +1,30 @@
+﻿using BookRentalSystem.Application.Interface;
+using BookRentalSystem.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookRentalSystem.Infrastructure.Repositories
+{
+    public class InMemoryBookRepository : IBookRepository
+    {
+        private readonly List<Book> _books = new();
+
+        public void Add(Book book)
+        {
+            _books.Add(book);
+        }
+
+        public Book GetById(int id)
+        {
+            return _books.FirstOrDefault(b => b.ID == id);
+        }
+
+        public List<Book> GetAll()
+        {
+            return _books;
+        }
+    }
+}
---
+++ Clean Architecture – Book Rental System/BookRentalSystem.Infrastructure/Repositories/InMemoryUserRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookRentalSystem.Application.Interface;
+using BookRentalSystem.Domain.Entities;
+
+namespace BookRentalSystem.Infrastructure.Repositories
+{
+    public class InMemoryUserRepository : IUserRepository
+    {
+        private readonly List<User> _users = new();
+
+        public void Add(User user)
+        {
+            _users.Add(user);
+        }
+
+        public User GetById(int id)
+        {
+            return _users.FirstOrDefault(u => u.ID == id);
+        }
+    }
+}
